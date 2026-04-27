728d0f661b77837541dfb425853565977492bc29
Doryan <doryan.planchon@hotmail.fr>
2026-04-27 11:13:31 +0200

    BDD

---
+++ bdd/script.sql
@@ -0,0 +1,85 @@
+-- ----------------------------------------------------------
+-- Script MYSQL pour mcd 
+-- ----------------------------------------------------------
+
+
+-- ----------------------------
+-- Table: especes
+-- ----------------------------
+CREATE TABLE especes (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(200) NOT NULL,
+  Feuillage VARCHAR(200) NOT NULL,
+  CONSTRAINT especes_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+-- ----------------------------
+-- Table: stade_dev
+-- ----------------------------
+CREATE TABLE stade_dev (
+  id INT NOT NULL AUTO_INCREMENT,
+  libelle VARCHAR(200) NOT NULL,
+  CONSTRAINT stade_dev_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+-- ----------------------------
+-- Table: quartiers
+-- ----------------------------
+CREATE TABLE quartiers (
+  id INT NOT NULL AUTO_INCREMENT,
+  quartier VARCHAR(200) NOT NULL,
+  secteur VARCHAR(200) NOT NULL,
+  CONSTRAINT quartiers_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+-- ----------------------------
+-- Table: etat
+-- ----------------------------
+CREATE TABLE etat (
+  id INT NOT NULL AUTO_INCREMENT,
+  libelle VARCHAR(200) NOT NULL,
+  CONSTRAINT etat_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+-- ----------------------------
+-- Table: situations
+-- ----------------------------
+CREATE TABLE situations (
+  id INT NOT NULL AUTO_INCREMENT,
+  libelle VARCHAR(200) NOT NULL,
+  CONSTRAINT situations_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+-- ----------------------------
+-- Table: arbre
+-- ----------------------------
+CREATE TABLE arbre (
+  id_arbre INT NOT NULL AUTO_INCREMENT,
+  X FLOAT NOT NULL,
+  Y FLOAT NOT NULL,
+  haut_tot FLOAT NOT NULL,
+  haut_tronc FLOAT NOT NULL,
+  diam_tronc FLOAT NOT NULL,
+  age_estime INT NOT NULL,
+  nb_diagnostic INT NOT NULL,
+  date_plantage DATE,
+  date_abattage DATE,
+  remarquable TINYINT(1) NOT NULL,
+  id INT NOT NULL,
+  id_especes INT NOT NULL,
+  id_etat INT NOT NULL,
+  id_quartiers INT NOT NULL,
+  id_situations INT NOT NULL,
+  CONSTRAINT arbre_PK PRIMARY KEY (id_arbre),
+  CONSTRAINT arbre_id_FK FOREIGN KEY (id) REFERENCES stade_dev (id),
+  CONSTRAINT arbre_id_especes_FK FOREIGN KEY (id_especes) REFERENCES especes (id),
+  CONSTRAINT arbre_id_etat_FK FOREIGN KEY (id_etat) REFERENCES etat (id),
+  CONSTRAINT arbre_id_quartiers_FK FOREIGN KEY (id_quartiers) REFERENCES quartiers (id),
+  CONSTRAINT arbre_id_situations_FK FOREIGN KEY (id_situations) REFERENCES situations (id)
+)ENGINE=InnoDB;
+
