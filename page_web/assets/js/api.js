c38cc1f161eda28f9506eac416b1dc74945f9942
Doryan <doryan.planchon@hotmail.fr>
2026-04-28 16:23:53 +0200

    remove file

--- page_web/assets/js/api.js
+++ page_web/assets/js/api.js
@@ -1,5 +1,5 @@
 function apiCall(endpoint, options) {
-  const API_URL = window.API_URL || 'http://localhost:8000';
+  const API_URL = process.env.API_URL;
   return fetch(API_URL + endpoint, options);
 }
 
